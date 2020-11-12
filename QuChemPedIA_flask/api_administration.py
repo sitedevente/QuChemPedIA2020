cf6a0107b0db0f37b06ce27bb4d6b385bbb6b2a3
salaniko <12Decembre1998>
2020-11-12 19:41:36 +0100

    Code commented

--- QuChemPedIA_flask/api_administration.py
+++ QuChemPedIA_flask/api_administration.py
@@ -12,17 +12,18 @@ from elasticsearch_dsl import Search, Q
 from elasticsearch_dsl.connections import connections
 
 
-###  Connexion Elasticsearch  ###
+# Elasticsearch client connection
 elasticClient = Elasticsearch(
     'https://yvwwd7r6g7:lue555jb9h@quchempedia-9079321169.eu-central-1.bonsaisearch.net')
 
 app = Flask(__name__)
 
 
-###  Route pour la recherche par formule  ###
+#  Route to search a molecule with its formula in Elasticsearch.
 @app.route('/api/search/<formula>', methods=['GET'])
 def search_molecule(formula):
-    """Return the list of molecules which contains the formula given."""
+
+    # Define the body for the request on the Elasticsearch client.
     body = {
         "query": {
             "match_phrase": {
@@ -31,76 +32,109 @@ def search_molecule(formula):
         }
     }
 
+    # Try the GET request on the Elasticsearch client.
+    # index is specific at the molecules documents.
     results = elasticClient.search(index='molecules', body=body)
+
+    # Return the results formatted in Json, e.g. status code = 200.
     return jsonify(results['hits']['hits']), 200
 
 
-###  Route pour retrouver une molécule avec son ID  ###
+# Route to retrieve a molecule with its ID in Elasticsearch.
 @app.route('/api/details/<id_mol>', methods=['GET'])
 def details_molecule(id_mol):
 
     try:
+        # Try the GET request on the Elasticsearch client.
+        # index and doc_type are specific at the molecules documents.
         results = elasticClient.get(
             index='molecules', doc_type='molecule', id=id_mol)
+
+        # Return the results formatted in Json, e.g. status code = 200.
         return jsonify(results), 200
     except elasticsearch.exceptions.NotFoundError:
+        # Return the error message, e.g. status code = 404.
         return jsonify({'Error': 'Molecule with id = \'' +
                         id_mol + '\' does not exists!'}), 404
 
 
-###  Route pour l'ajout d'une molécule  ###
+# Route to add a new molecule in Elasticsearch.
 @app.route('/api/add', methods=['POST'])
 def add_molecule():
 
+    # Define the body with the Json contained in the POST request.
     body = request.json
 
-    if (body == None):
-    	return jsonify({'Error': 'There is no body provided for the molecule!'}), 404
+    if (body is None):
+        # Return the error message, e.g. status code = 404.
+        return jsonify(
+            {'Error': 'There is no body provided for the molecule!'}), 404
 
+    # Try the INDEX request on the Elasticsearch client.
+    # index and doc_type are specific at the molecules documents.
     results = elasticClient.index(
         index='molecules', doc_type='molecule', body=body)
+
+    # Call the function for the log file creation
     add_log_file(results["_id"])
-    return jsonify(results)
+
+    # Return the results formatted in Json, e.g. status code = 200.
+    return jsonify(results), 200
 
 
-###  Route pour la suppression d'une molécule  ###
+# Route to delete a molecule with its ID in Elasticsearch.
 @app.route('/api/delete/<id_mol>', methods=['DELETE'])
 def delete_molecule(id_mol):
 
     try:
+        # Try the DELETE request on the Elasticsearch client.
+        # index and doc_type are specific at the molecules documents.
         results = elasticClient.delete(
             index='molecules', doc_type='molecule', id=id_mol)
+
+        # Call the function to delete the molecule log file.
         delete_log_file(id_mol)
+
+        # Return the results formatted in Json, e.g. status code = 200.
         return jsonify(results), 200
     except elasticsearch.exceptions.NotFoundError:
+        # Return the error message, e.g. status code = 404.
         return jsonify({'Error': 'Molecule with id = \'' +
                         id_mol + '\' does not exists!'}), 404
 
 
-# Fonction pour la création d'un fichier de log lors de l'ajout de
-# molécules  ###
+# Function for the creation of the log file when a new molecule
+# is added to the database.
 def add_log_file(id_mol):
 
+    # Define the root path for log files.
     root_path = 'data_dir/'
     log_path = ''
 
+    # Parse the molecule id and define the path of the log file.
     for char in id_mol:
         log_path += char
         log_path += '/'
 
     root_path += log_path
+
+    # Create directories for the log file.
     os.makedirs(root_path)
 
+    # Create the log file.
     log_file = open(root_path + "data.log", "x")
     log_file.write(id_mol)
 
 
-###  Fonction pour la suppression d'un fichier de log  ###
+# Function for the suppression of the log file when a molecule
+# is deleted from the database.
 def delete_log_file(id_mol):
 
+    # Define the root path for log files.
     root_path = 'data_dir/'
     log_path = ''
 
+    # Parse the molecule id and define the path of the log file.
     for char in id_mol:
         log_path += char
         log_path += '/'
@@ -108,14 +142,16 @@ def delete_log_file(id_mol):
     root_path += log_path
     log_file_path = root_path + 'data.log'
 
+    # Delete the existing log file and empty directories.
     if os.path.exists(log_file_path):
         os.remove(log_file_path)
         delete_empty_path(root_path[:-1])
 
 
-###  Fonction pour supprimer les dossiers vides  ###
+# Function for the suppresion of empty folders.
 def delete_empty_path(path):
 
+    # Delete the directory if is empty and call the function recursively.
     if len(os.listdir(path)) == 0:
         os.rmdir(path)
         delete_empty_path(path[:-2])
