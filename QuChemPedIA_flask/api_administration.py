66fe5c0f417427ae23e19894df13b0ea6a499f6e
salaniko <12Decembre1998>
2020-11-24 16:10:07 +0100

    Log file creation modified

--- QuChemPedIA_flask/api_administration.py
+++ QuChemPedIA_flask/api_administration.py
@@ -1,4 +1,4 @@
-import os
+import os, shutil, json
 
 import flask
 from flask import Flask, jsonify, request
@@ -71,21 +71,53 @@ def details_molecule(id_mol):
 @app.route('/api/add', methods=['POST'])
 def add_molecule():
 
-    # Define the body with the Json contained in the POST request.
-    body = request.json
+    # Define the files given in the POST request.
+    files = request.files
 
-    if (body is None):
+    # Check if the Json is given in the POST request.
+    if 'mol_json' not in files:
         # Return the error message, e.g. status code = 400.
         return jsonify(
             {'error': 'You must provide a body for the molecule!'}), 400
+
+    body = files["mol_json"].read().decode("utf-8")
+
+    # Check if the log file is absent from the POST request.
+    if 'mol_log' not in files:
+        body_json = json.loads(body)
+
+        # Reverse split on the right Json line in order 
+        # to get the source path and the file name.
+        log_file_path = body_json['metadata']['log_file'].rsplit("/", 1)
+
+        # Generate the modified Json with the new path.
+        body_json['metadata']['log_file'] = log_file_path[1]
+        body = json.dumps(body_json)
 
     # Try the INDEX request on the Elasticsearch client.
     # index and doc_type are specific at the molecules documents.
     results = elasticClient.index(
         index='molecules', doc_type='molecule', body=body)
 
-    # Call the function for the log file creation
-    add_log_file(results["_id"])
+    # Check if the log file is absent from the POST request.
+    try:
+        if 'mol_log' in files:
+            # Define the log file name and data from the POST request.
+            log_file_data = files["mol_log"].read().decode("utf-8")
+            log_file_name = files["mol_log"].filename
+
+            # Call the function for the log file creation with log file.
+            add_log_file_from_param(results["_id"], 
+                log_file_name, log_file_data)
+        else:
+            # Call the function for the log file creation with log file source path.
+            add_log_file_from_json(results["_id"], log_file_path)
+    except Exception as err:
+        # If an error is raised, we delete the molecule and print the error message, e.g. status code 500.
+        elasticClient.delete(
+            index='molecules', doc_type='molecule', id=results["_id"])
+        return jsonify(
+            {'error': 'An error occured during the log file creation! ## ' + str(err)}), 500
 
     # Return the results formatted in Json, e.g. status code = 201.
     return jsonify(results), 201
@@ -112,9 +144,38 @@ def delete_molecule(id_mol):
                         id_mol + '\' does not exists!'}), 404
 
 
-# Function for the creation of the log file when a new molecule
-# is added to the database.
-def add_log_file(id_mol):
+# Function for the creation of the log file when a new molecule is added
+# to the database and when the log file is given in the POST request.
+def add_log_file_from_param(id_mol, log_file_name, log_file_data):
+
+    # Define the root path for log files.
+    log_path = ''
+
+    # Parse the molecule id and define the path of the log file.
+    for char in id_mol:
+        log_path += char
+        log_path += '/'
+
+    path = root_path_log_files + log_path
+
+    # Create directories for the log file.
+    os.makedirs(path)
+
+    # Create the log file with the data.
+    try:
+        log_file = open(path + log_file_name, "x")
+        for line in log_file_data:
+            log_file.write(line)
+    except OSError as err:
+        # Delete the path created just before and raise an error if
+        # the file creation failed.
+        delete_empty_path(path)
+        raise err
+
+
+# Function for the creation of the log file when a new molecule is added
+# to the database and when the log file is not given in the POST request.
+def add_log_file_from_json(id_mol, log_file_src):
 
     # Define the root path for log files.
     log_path = ''
@@ -130,8 +191,15 @@ def add_log_file(id_mol):
     os.makedirs(path)
 
     # Create the log file.
-    log_file = open(path + "data.log", "x")
-    log_file.write(id_mol)
+    try:
+        log_file = open(path + log_file_src[1], "x")
+        shutil.copyfile(log_file_src[0] + '/' + log_file_src[1], path + log_file_src[1])
+    except OSError as err:
+        # Delete the file and path created just before and raise an error if
+        # the file creation or the file copy failed.
+        os.remove(path + log_file_src[1])
+        delete_empty_path(path)
+        raise err
 
 
 # Function for the suppression of the log file when a molecule
