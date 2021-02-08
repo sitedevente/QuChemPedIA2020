99d14ad34c901cdd565c457d2e2c1253f7df42c5
Alexandre BOUDET <alexandreboudet35@gmail.com>
2021-02-08 22:22:20 +0100

    Fix url modifications path logfile

--- Web/js/detail.js
+++ Web/js/detail.js
@@ -515,7 +515,7 @@ request.onreadystatechange = function() {
             let id = response.id;
             let path = id.split('');
             let name = response.data.metadata.log_file;
-            let lien = "/reboot/mnt/data_dir/";
+            let lien = "file:////mnt/data_dir/";
 
             //On sépare chaque lettre de l'id par un / pour obtenir le path ou se trouve le logfile
             for(let i = 0;i<path.length;i++){
