80d22669a83a325671ab100caf268b035ac7491a
Benoit <benoit@ansible>
2021-02-16 09:48:29 +0100

    [HOTFIX] try to fix search URL onload

--- Web/js/Search.js
+++ Web/js/Search.js
@@ -227,7 +227,7 @@ function searchURL() {
 
   if (type != null && q != null && page != null && entrie != null) {
     $("#query").val(q);
-    $("#id_typeQuery").val(type).change();
+    $('#id_typeQuery" option[value="' + type + '"]').prop('selected', true);
     search(page, entrie);
   }
 }
